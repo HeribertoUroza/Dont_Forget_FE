996dd65a2e8d5a7e76c9c105132ecd841259b183
Heriberto Uroza <heribertouroza@pursuit.org>
2020-01-01 18:18:36 -0500

    added input feild with submit btn. Css needed and functionality

--- src/components/Lists/Lists.js
+++ src/components/Lists/Lists.js
@@ -6,10 +6,12 @@ import './lists.css'
 function List (props){
     console.log('list', props.list_items)
     let listItemArr = props.list_items
-
-    return (
+    //listItemArr.push({name:'test', is_completed: 'TRUE'})
+    return ( 
         <>  
-
+            <div className='input_container' >
+                <input className='submit_field' placeholder='Add a ToDo' /><button className='submit_btn'>+</button>
+            </div>
             {
                 listItemArr.map((e,i) => {
                     console.log(e)
