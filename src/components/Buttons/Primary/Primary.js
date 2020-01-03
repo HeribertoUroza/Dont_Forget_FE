e716ad43bed8b665d35e36e73060fc2425fa068c
Heriberto Uroza <heribertouroza@pursuit.org>
2020-01-02 22:43:43 -0500

    added another google font for primary btn and added color to match mock ups

--- src/components/Buttons/Primary/Primary.js
+++ src/components/Buttons/Primary/Primary.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+// CSS
+import './primary.css'
+
 function Primary(props) {
     
     return(
         <> 
-            <button onClick={e => { console.log('click')}}>{ props.btn_name }</button>
+            <div className='primary_btn_container'>
+                <button className='primary_btn' onClick={e => { console.log('click') }}>{props.btn_name}</button>
+            </div>
         </>
     )
 };
